3796c031fe6054c188e9cdbd3fc16d48d8e5dfb8
Michael Christenson II <me@xorsense.nl>
2022-06-08 11:35:17 -0400

    Add project files.

---
+++ RecipeCli/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace RecipeCli
+{
+    class Program
+    {
+        /*
+         * Usage:
+         *  recipecli add <name> <description>
+         *  recipecli list
+         *  recipecli show <name>
+         */
+        static void Main(string[] args)
+        {
+            bool run = true;
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Press the Escape key to end the program...");
+            while (run)
+            {
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                var key = consoleKeyInfo.Key;
+
+                if (key == ConsoleKey.Escape)
+                {
+                    run = false;
+                }
+            }
+        }
+    }
+}
